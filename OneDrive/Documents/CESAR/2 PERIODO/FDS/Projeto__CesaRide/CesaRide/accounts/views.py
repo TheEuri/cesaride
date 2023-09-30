558351aff3a7c53902c6b9c4710c48656191e9b1
Alec Lucas Ferraz Eurivaldo <act@cesar.school>
2023-09-29 21:45:48 -0300

    Função Incial (incompleta)

---
+++ OneDrive/Documents/CESAR/2 PERIODO/FDS/Projeto__CesaRide/CesaRide/accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from . forms import CustomUserCreationForm
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registro/signup.html', {'form':form})
+
+
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registro/signin.html', {'form':form})
+
