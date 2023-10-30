f1f7e70d54012df2c0505e74d9eaa6e978b40c5c
Eurivaldo Filho <eurivaldojvf@gmail.com>
2023-10-30 14:17:33 -0300

    Create urls.py

---
+++ CesaRide/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('signup/', views.signup,name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('driver_home/', views.driver_home,name='pagina_motorista'),
+    path('passenger_home/',views.passenger_home,name='pagina_passageiro'),
+    path('choose_role/',views.choose_role,name='choose_role'),
+    
+
+]
