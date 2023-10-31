5c73870c34f9e79337285c059a3eb15401aebea7
Eurivaldo Filho <eurivaldojvf@gmail.com>
2023-10-30 23:02:36 -0300

    test ci

--- CesaRide/accounts/tests.py
+++ CesaRide/accounts/tests.py
@@ -10,6 +10,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=chrome_options)
 
 
